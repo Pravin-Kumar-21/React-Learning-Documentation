da902594a763b2793e01857cffdd002ab60c8d9a
Pravin <pravinkumar2108@gmail.com>
2024-07-26 22:29:07 +0530

    learned functional component in react

---
+++ god-rolodex/src/App.js
@@ -0,0 +1,26 @@
+import logo from './logo.svg';
+import './App.css';
+// Remeber this is a functional Component in React .... 
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+// next we will learn what is a class component in react and how it is used
+
+export default App;
