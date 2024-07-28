84cde252a2cd8d121753dc253c5df44325678a4f
Pravin <pravinkumar2108@gmail.com>
2024-07-28 20:10:08 +0530

    setstate in react

--- god-rolodex/src/App.js
+++ god-rolodex/src/App.js
@@ -5,14 +5,24 @@ import logo from './logo.svg';
 import './App.css';
 class App extends Component {  // now in case of a functional component we just return the HTML but in case of class component you 
   // have to use render() and then return the html template
+  constructor(){
+    super();
+   this.state ={
+    Name : "Pravin",
+    role: "Full Stack Developer",
+   };
+  }
   render(){
-
     return (
       <div className="App">
         <header className="App-header">
-          <p>
-           Hello React Developer's ? What are you building...
-          </p>
+          <p className='title'>Hello React Developers ? What are you building...</p>
+          <p>My name is {this.state.Name} , I work as a {this.state.role}  </p>
+          <button onClick={
+            ()=> {
+              this.setState({Name: "Pravin Kumar"});
+            }
+            }> Click Here </button>
         </header>
       </div>
     );
