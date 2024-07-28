102953e6cba8db6b21cf7a612bea6f57bda5ba35
Pravin <pravinkumar2108@gmail.com>
2024-07-28 22:14:00 +0530

    objects in setstate

--- god-rolodex/src/App.js
+++ god-rolodex/src/App.js
@@ -8,8 +8,8 @@ class App extends Component {  // now in case of a functional component we just 
   constructor(){
     super();
    this.state ={
-    Name : "Pravin",
-    role: "Full Stack Developer",
+    Name : {firstname:"Pravin", lastname: "Kumar"}, // i have use an object here so in usestate i will also *
+    role: "Full Stack Developer", // suppose here i can use here the object also 
    };
   }
   render(){
@@ -17,12 +17,14 @@ class App extends Component {  // now in case of a functional component we just 
       <div className="App">
         <header className="App-header">
           <p className='title'>Hello React Developers ? What are you building...</p>
-          <p>My name is {this.state.Name} , I work as a {this.state.role}  </p>
-          <button onClick={
-            ()=> {
-              this.setState({Name: "Pravin Kumar"});
-            }
-            }> Click Here </button>
+          <p>My name is {this.state.Name.firstname} {this.state.Name.lastname} , I work as a {this.state.role}  </p>
+          <button onClick={()=> 
+          {
+              this.setState({
+                Name:{firstname:"Navin",lastname:"Kumar"}});
+                console.log(this.state)
+          }
+          }> Click Here </button>
         </header>
       </div>
     );
