a5de5cdfe7095afffa7a7a6e5a1ea3672af05e74
Pravin <pravinkumar2108@gmail.com>
2024-07-28 23:50:08 +0530

    making setstate uptodate

--- god-rolodex/src/App.js
+++ god-rolodex/src/App.js
@@ -1,5 +1,11 @@
 // now this is how we are going to use class component in react 
 // first import component from react 
+
+// Note the setstate is asychronously done . To make the set state up to date instead of creating a function inside
+// of the set state we pass a function inside the setstate
+// setstate(() => {}, () => {}) the first function is the arugument function and the second function is the callback function
+
+// like this 
 import { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
@@ -20,11 +26,17 @@ class App extends Component {  // now in case of a functional component we just 
           <p>My name is {this.state.Name.firstname} {this.state.Name.lastname} , I work as a {this.state.role}  </p>
           <button onClick={()=> 
           {
-              this.setState({
-                Name:{firstname:"Navin",lastname:"Kumar"}});
-                console.log(this.state)
-          }
-          }> Click Here </button>
+              this.setState(() =>{
+                return { 
+                  Name:{firstname:"Navin",lastname:"Kumar"}
+                };
+              }, // first function completion which is the argument 
+              () => {
+                  console.log(this.state);
+                } // second function which is the callback function
+              );
+          }} // Now the setstate() will be up to date
+          > Click Here </button>
         </header>
       </div>
     );
