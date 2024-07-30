4a87a367b7b09ac40aa7bb059ea59d1fd4c72143
Pravin <pravinkumar2108@gmail.com>
2024-07-30 23:56:21 +0530

    Storing original Data

--- god-rolodex/src/App.js
+++ god-rolodex/src/App.js
@@ -10,31 +10,49 @@ import { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 class App extends Component {  
-  constructor(){
+  constructor(){  // ""* 1. First this.state will Work   ""
     super();
    this.state ={ // this we have created an array of user 
     user: [],
-   };
+    searchString: '' //we have intialized the string that will store the value of the search users
+  };
+  console.log("1"); // how react class component code flows 
   }
-  componentDidMount(){
+  componentDidMount(){ // ""* 3. Third the compnentdidmount will run""
+    console.log("3");
     fetch('https://jsonplaceholder.typicode.com/users') // this will return a promise 
     .then((response) => response.json())    // it will also return a promise 
     .then((users) => this.setState(() => { // now we use setstate to save the user json data format
-      return { user : users };
+      console.log(users);
+      return { user : users };      // copying user and creating objects one by one
     },
     () => {
-      console.log(this.state);
+      // console.log(this.state);
     }
   ));
   };
-  render(){
+  render(){ // ""* second The render state will work ""
+    const filterUsers = this.state.user.filter((user) => {
+            return user.name.toLocaleLowerCase().includes(this.state.searchString);
+        });
+    console.log("2");
     return (
       <div className="App">
+      <input className='search-box' 
+        type='search' 
+        placeholder='Search Users' 
+        onChange={(event) =>{
+          const searchString = event.target.value.toLocaleLowerCase();
+          this.setState(() => {
+            return {searchString};
+          });
+        }}
+        />
         <header className="App-header">
-          {this.state.user.map((g) => {
+          {filterUsers.map((g) => {
             return (
             <div  key={g.id}>
-              <p>{g.username}</p>
+              <p>{g.name}</p>
             </div>
           );
           })
